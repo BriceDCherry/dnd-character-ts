372a0852786c34f0c2b8c122db6324eaa971790a
bcherry4 <bcherry4@ford.com>
2022-05-11 10:57:52 -0600

    Add solution

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,1 +1,43 @@
-export class DnDCharacter {}
+export class DnDCharacter {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+    hitpoints: number;
+
+
+constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10+DnDCharacter.getModifierFor(this.constitution)
+}
+
+static numberGenerator() {
+    return Math.ceil(Math.random() * 6)
+}
+
+static topThreeRolls() {
+    const diceRoll = [];
+    while (diceRoll.length < 4) {
+        diceRoll.push(this.numberGenerator())
+    }
+    const lowestDice = Math.min(...diceRoll);
+    return diceRoll.filter(roll => roll != lowestDice);
+}
+
+static generateAbilityScore() {
+    const topThree = this.topThreeRolls()
+        .reduce((runningTotal, nextNumber) => runningTotal + nextNumber, 0)
+    return topThree
+    }
+
+static getModifierFor(constitution: number) {
+    return Math.floor((constitution -10) / 2)
+}
+}
